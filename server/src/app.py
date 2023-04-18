7cf1e07ce9d0dd097909de23de9dfc75ab48af3f
Sabeer <msabeerasad@gmail.com>
2023-04-18 16:00:31 +0500

    Started working to stream SOME data
    Set the stage to initialize Websocket API for Flask server + Flutter client

--- server/src/app.py
+++ server/src/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+async_mode = None
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
 
-@app.route("/")
-def hello():
-    return ''
+
+
+
+if __name__ == "__main__":
+    socketio.run(app)
