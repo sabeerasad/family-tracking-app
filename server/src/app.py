8bc468c758aad7709c15b06b88b2d48b9907f2ab
Sabeer <msabeerasad@gmail.com>
2023-04-21 12:33:32 +0500

    Tryna get socket to work ong it ain't working

--- server/src/app.py
+++ server/src/app.py
@@ -1,14 +1,48 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+# copied from https://medium.com/swlh/implement-a-websocket-using-flask-and-socket-io-python-76afa5bbeae1
+from flask import Flask, render_template, session, copy_current_request_context
+from flask_socketio import SocketIO, emit, disconnect
+from threading import Lock
 
-app = Flask(__name__)
+
 async_mode = None
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
 
-@app.route("/")
-def hello():
-    return render_template('index.html', sync_mode=socketio.async_mode)
 
-if __name__ == "__main__":
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socketio.async_mode)
+
+
+@socketio.on('my_event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+@socketio.on('my_broadcast_event', namespace='/test')
+def test_broadcast_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+
+@socketio.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    @copy_current_request_context
+    def can_disconnect():
+        disconnect()
+
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']},
+         callback=can_disconnect)
+
+
+if __name__ == '__main__':
     socketio.run(app, debug=True)
