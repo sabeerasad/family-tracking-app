498a64343cfdf0389f35dd01e099dcf0df09f90d
Sabeer <msabeerasad@gmail.com>
2023-04-23 16:55:48 +0500

    Socketing progress
    (mostly) Figured out how the copied code works; what the hell is 'cb()' in the Javascript???

--- server/src/static/scripts/script.js
+++ server/src/static/scripts/script.js
@@ -2,27 +2,27 @@
 $(document).ready(function() {
 
     namespace = '/test';
-    var socket = io(namespace);
+    var socketio = io(namespace);
 
-    socket.on('connect', function() {
-        socket.emit('my_event', {data: 'connected to the SocketServer...'});
+    socketio.on('connect', function() {
+        socketio.emit('my_event', {data: 'connected to the SocketServer...'});
     });
 
-    socket.on('my_response', function(msg, cb) {
+    socketio.on('my_response', function(msg, cb) {
         $('#log').append('<br>' + $('<div/>').text('logs #' + msg.count + ': ' + msg.data).html());
-        if (cb)
-            cb();
+        // if (cb)
+        //     cb();
     });
     $('form#emit').submit(function(event) {
-        socket.emit('my_event', {data: $('#emit_data').val()});
+        socketio.emit('my_event', {data: $('#emit_data').val()});
         return false;
     });
     $('form#broadcast').submit(function(event) {
-        socket.emit('my_broadcast_event', {data: $('#broadcast_data').val()});
+        socketio.emit('my_broadcast_event', {data: $('#broadcast_data').val()});
         return false;
     });
     $('form#disconnect').submit(function(event) {
-        socket.emit('disconnect_request');
+        socketio.emit('disconnect_request');
         return false;
     });
 });
