74b452a50cb3bbea2d61216f919394ff7cfef1f8
Sabeer Asad <msabeerasad@gmail.com>
2023-07-10 16:53:25 +0500

    2-way communication of mobileCount and webCount establised.
    Next steps: centralize the data into (temporary) session database, and display webCount on mobile_app

--- lib/client/client.dart
+++ lib/client/client.dart
@@ -1,24 +1,28 @@
 import 'package:socket_io_client/socket_io_client.dart';
 
-final Socket socket = io(
+final socket = io(
   'http://10.0.2.2:5000/test', // ? something something use 10.0.2.2 to "loopback" to 127.0.0.1 https://developer.android.com/studio/run/emulator-networking
   OptionBuilder().setTransports(['websocket']).build(),
 );
 
-void connectSocket() => socket.onConnect((_) {
-      print("CONNECTION ESTABLISHED");
-      socket.emit('connection', {
-        'data': 'Flutter client connected',
-        'client': 'flutter',
-      });
+void connectSocket() {
+  socket.onConnect((_) {
+    print("CONNECTION ESTABLISHED");
+    socket.emit('connection', {
+      'data': 'Flutter client connected',
+      'client': 'flutter',
     });
+
+    // * Listeners
+    socket.on('response-to-web', (data) {
+      // TODO: construct handler function to display webCount in app
+    });
+  });
+}
 
 void sendSocketCounter(int counter) => socket.emit(
       'update-from-mobile',
       {'data': counter},
     );
 
-receiveSocketCounter() => socket.on('response-to-web', (data) {
-      // TODO: 2-way communication of `counter` (both, web and mobile counters) with centralized data storage in Flask session
-      print(data['webCount']);
-    });
+// TODO: 2-way communication of `counter` (both, web and mobile counters) with centralized data storage in Flask session
