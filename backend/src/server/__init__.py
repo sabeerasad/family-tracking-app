0586bd42c408e8947f0b3fda4704291934c032da
Sabeer Asad <msabeerasad@gmail.com>
2023-07-23 14:49:21 +0500

    Refactoring Flask. Connection established to MySQL database. Have yet to test that connection

--- backend/src/server/__init__.py
+++ backend/src/server/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '' # ! DON'T SHARE SECRET KEY IN PROD (use environment variables)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:81672943@localhost/{DB_NAME}' # ! use environment variables for username and password in prod
+    db.init_app(app)
 
     from .routes import routes
     from .auth import auth
